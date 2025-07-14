aab74a596d8d172363ce73f354e12f366c9ac3b7
thunderbolt <dhruvagarwal@jklu.edu.in>
2025-07-14 12:23:12 +0530

    Implementation of middlewares

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
-
-
-
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
+// connected to mongodb
 const app = express();
+
+// In BUild middleWare
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+
+
+// Routes
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+
+// Error handling using middleWare
+// This is custom build we have defined  
+app.use(errorMiddleware);
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('Welcome to the Subscription Tracker API ! ' );
